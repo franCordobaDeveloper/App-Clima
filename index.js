b4647c77cb17d649dd35c20697324d641274fa7f
FranDev <fran.cordoba.developer@gmail.com>
2023-05-03 22:16:11 -0300

    App Clima Finalizado

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Busquedas = require("./models/busquedas");
 require('dotenv').config();
 
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
+
+const { leerInput, inquirerMenu, pausa, listarLugares } = require("./helpers/inquirer");
 
 
 
@@ -21,25 +22,49 @@ const main = async () => {
         switch ( opt ) {
             case 1:
                 // Mostrar mensaje
-                const lugar = await leerInput('Ingrese ciudad a buscar: ');
-                await busquedas.ciudad( lugar );
+                const termino = await leerInput('Ingrese ciudad a buscar: ');
+                
                 // buscar los lugares
-
-
+                const lugares = await busquedas.ciudad( termino );
+               
                 // Seleccionar el lugar 
+                const id = await listarLugares( lugares ); 
+                if ( id === '0' ) continue;
 
+               
+
+                // console.log({ id });
+                const lugarSel = lugares.find(l => l.id === id );
+
+                // Guardar en DB
+                busquedas.agregarHistorial( lugarSel.nombre );
+                
+                // console.log( lugarSel );
+                
                 // Datos del clima 
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
 
                 // Mostrar resultados
+                console.clear();
                 console.log('\nInformación de la ciudad\n'.green);
-                console.log('Ciudad:', );
-                console.log('Lat:', );
-                console.log('Lng:', );
-                console.log('Temperatura:', );
-                console.log('Mínima:', );
-                console.log('Máxima:', );
-                break;
-        
+                console.log('Ciudad:', lugarSel.nombre );
+                console.log('Lat:', lugarSel.lat );
+                console.log('Lng:', lugarSel.lng );
+                console.log('Como esta el clima:', clima.desc)
+                console.log('Temperatura:', clima.temp);
+                console.log('Mínima:', clima.min);
+                console.log('Máxima:', clima.max);
+                console.log('Humedad:', clima.humedad);
+                console.log('Nivel del mar:', clima.nivel_mar);
+            break;
+            
+            case 2:
+                busquedas.historialCapitalizado.forEach( (lugar , i) => {
+                    const idx = `${ i + 1 }`.green;
+                    console.log( `${ idx } ${ lugar }`);
+                })
+            break;
+
             default:
                 break;
         }
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,11 +1,15 @@
+const fs = require('fs');
+
 const axios = require('axios');
 
 class Busquedas {
 
-    historial = ['']
+    historial = [];
+    dbPath = './db/database.json';
 
     constructor() {
         // TODO leer DB si existe
+        this.leerDB();
     }
 
     get paramsMapbox () {
@@ -14,6 +18,23 @@ class Busquedas {
             'limit': 5,
             'language': 'es'
         }
+    }
+
+    get paramsOpenWeather () {
+        return {
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
+        }
+    }
+
+    get historialCapitalizado() {
+        return this.historial.map( lugar => {
+            let palabras = lugar.split(" ");
+            palabras = palabras.map( p => p[0].toUpperCase() + p.substring(1) );
+
+            return palabras.join(" ");
+       }); 
     }
 
     async ciudad( lugar = '' ) {
@@ -26,9 +47,14 @@ class Busquedas {
             });
 
             const resp = await instance.get();
-            console.log( resp.data );
+            return resp.data.features.map( lugar => ({
 
-            return [];
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1]
+                
+            }));
             
         } catch( error ) {
             return [];
@@ -36,6 +62,71 @@ class Busquedas {
 
         return []; // retornar los lugares
     }
+
+    async climaLugar( lat, lon ) {
+
+        try {
+        
+            // instancia de axios
+            const instanceClima = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: {...this.paramsOpenWeather, lat, lon }
+            })
+            // resp extraer informacion de la data
+            const respClima = await instanceClima.get();
+            const { weather, main } = respClima.data;
+
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp,
+                nivel_mar: main.sea_level,
+                humedad: main.humidity
+            }
+
+        } catch ( error ) {
+            console.log( error );
+        }
+    }
+
+    agregarHistorial( lugar = '' ) {
+
+        // TODO: Prevenir duplicados
+        if ( this.historial.includes( lugar.toLocaleLowerCase() ) ) {
+            return;
+        }
+
+        this.historial = this.historial.splice(0,5);
+
+        this.historial.unshift( lugar.toLocaleLowerCase() );
+
+        // Grabar en DB 
+        this.guardarDB();
+    }
+
+    guardarDB () {
+
+        const payload = {
+            historial: this.historial
+        };
+
+        fs.writeFileSync( this.dbPath, JSON.stringify( payload ) );
+    }
+
+    leerDB () {
+
+        if( !fs.existsSync( this.dbPath ) ) return ;
+     
+        const info = fs.readFileSync( this.dbPath, {
+            encoding: 'utf-8'
+        });
+
+        const data = JSON.parse( info );
+
+        this.historial = data.historial;
+    }
+
 }
 
 
